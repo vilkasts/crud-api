b47d2b7e1ec1f903fcd130406275e2cd6784c013
vilkasts <vilkasts@gmail.com>
2024-10-21 22:29:05 +0200

    refactor: final refactoring

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import type { IncomingMessage, ServerResponse, Server } from 'node:http';
 import { createServer } from 'node:http';
-import { cpus } from 'node:os';
 import { config } from 'dotenv';
 
 import { requestHandler } from './helpers/request-handler';
@@ -8,10 +7,7 @@ import { usersData } from './database';
 
 config();
 
-const coresQuantity: number = cpus().length;
-const PORT: string = process.env.PORT ?? '4000';
-
-console.log('coresQuantity', coresQuantity);
+const PORT: number = parseInt(process.env.PORT ?? '4000');
 
 const server: Server<typeof IncomingMessage, typeof ServerResponse> =
   createServer((req: IncomingMessage, res: ServerResponse): void =>
